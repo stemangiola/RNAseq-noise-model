42d3684b34a92b9958f630d01232d260eeb49078
martin_cerny <cerny.m@gmail.com>
2018-10-06 18:51:59 +0200

    Work towards other R simulators

--- R/simulators.R
+++ R/simulators.R
@@ -3,8 +3,8 @@
 #' @param draw A vector of item counts
 #' @param k Number of items to be selected
 resample_draw = function(draw, k) {
-  if(k <= sum(draw)) {
-    stop("The number reads to sample (k) has to be larger than the sum of draw")
+  if(k >= sum(draw)) {
+    stop("The number reads to sample (k) has to be smaller than the sum of draw")
   }
   G = length(draw)
   positions = c(0, cumsum(draw))
@@ -23,16 +23,41 @@ resample_draw = function(draw, k) {
   counts
 }
 
+#' Generate a vector of size N that sums to zero and marginal
+#' distributions for all elements are N(0,1)
+#' Original code due to andre.pfeuffer
+#' https://discourse.mc-stan.org/t/test-soft-vs-hard-sum-to-zero-constrain-choosing-the-right-prior-for-soft-constrain/3884/31
+rnorm_sum_to_zero = function(N) {
+  Q_r = numeric(2 * N)
+  for(i in 1:N) {
+    Q_r[i] = -sqrt((N-i)/(N-i + 1))
+    Q_r[i+N] = 1/sqrt((N - i) * (N - i + 1))
+  }
+
+  x_raw = rnorm(N - 1, 0, 1/sqrt(1-1/N))
+  x = numeric(N)
+  x_aux = 0
+
+  for(i in 1:(N-1)) {
+    x[i] = x_aux + x_raw[i] * Q_r[i]
+    x_aux = x_aux + x_raw[i] * Q_r[i + N]
+  }
+  x[N] = x_aux
+  x
+}
+
 #' @param generator_type either "dm" or "nb_resample" (should be equivalent in practice, but this lets us test)
 #' @param model_type either "dm" or "nb" which tells the Stan model which method to use (once again should be equivalent but this lets us test numerical and other issues)
-generate_data_dirichlet_multinomial <- function(G, sums, lambda_raw_prior,  theta_given = TRUE, alpha_given = TRUE,
-                                  alpha_prior_log_mean = log(5000), alpha_prior_log_sd = 1,
+generate_data_dirichlet_multinomial_base <- function(G, sums, lambda_raw_prior,  theta_given = TRUE, alpha_given = TRUE,
+                                  alpha_prior_log_mean = 3, alpha_prior_log_sd = 1,
                                   theta_prior_log_mean = log(5), theta_prior_log_sd = 1, generator_type = "dm",
                                   model_type = "nb") {
   N = length(sums)
   counts = array(-1, c(N, G))
 
-  lambda_raw = MCMCpack::rdirichlet(1, rep(lambda_raw_prior,G))
+  #lambda_raw = MCMCpack::rdirichlet(1, rep(lambda_raw_prior,G))
+  lambda_raw = rlnorm(G, 0, lambda_raw_prior)
+  lambda_raw = lambda_raw / sum(lambda_raw)
 
   theta = rlnorm(1,theta_prior_log_mean, theta_prior_log_sd)
   alpha = rlnorm(1, alpha_prior_log_mean, alpha_prior_log_sd)
@@ -41,10 +66,12 @@ generate_data_dirichlet_multinomial <- function(G, sums, lambda_raw_prior,  thet
     single_draw_func <- single_draw_dirichlet_multinom
   } else if(generator_type == "nb_resample") {
     single_draw_func <- single_draw_nb_fano_resample
+  } else {
+    stop("Invalid generator_type")
   }
 
   for(n in 1:N) {
-    counts[n,] = single_draw_func(lambda_raw * alpha, theta, sums[n])
+    counts[n,] = single_draw_func(lambda_raw * sums[n] * alpha, sums[n], theta)
   }
 
   data = list(
@@ -93,10 +120,28 @@ single_draw_dirichlet_multinom <- function(lambda, k, theta) {
 }
 
 
-single_draw_nb_fano_resample <- function(lambda, k, theta) {
+single_draw_nb_fano_resample <- function(lambda, k, theta, num_trials = 100) {
   G = length(lambda)
   for(i in 1:100) {
     raw_counts_draw = rnbinom(G, mu = lambda, size = lambda / theta)
+    if(sum(raw_counts_draw) >= num_trials) {
+      break;
+    }
+    if(i == 5) {
+      warning("Neded over 5 draws")
+    }
+  }
+  if(sum(raw_counts_draw) < k) {
+    stop("Could not draw large enough")
+  }
+
+  resample_draw(raw_counts_draw, k)
+}
+
+single_draw_nb_resample <- function(lambda, k, phi, num_trials = 100) {
+  G = length(lambda)
+  for(i in 1:100) {
+    raw_counts_draw = rnbinom(G, mu = lambda, size = phi)
     if(sum(raw_counts_draw) >= num_trials) {
       break;
     }
@@ -108,5 +153,59 @@ single_draw_nb_fano_resample <- function(lambda, k, theta) {
     stop("Could not draw large enough")
   }
 
-  resample_draw(G, raw_counts_draw, num_trials)
+  resample_draw(raw_counts_draw, k)
+}
+
+generate_data_nb_multinomial <- function(G, sums, lambda_raw_prior,  phi_given = TRUE, alpha_given = TRUE,
+                                                     alpha_prior_log_mean = 3, alpha_prior_log_sd = 1,
+                                                     phi_prior_log_mean = log(5), phi_prior_log_sd = 1) {
+  N = length(sums)
+  counts = array(-1, c(N, G))
+
+  #lambda_raw = MCMCpack::rdirichlet(1, rep(lambda_raw_prior,G))
+  lambda_raw = rlnorm(G, 0, lambda_raw_prior)
+  lambda_raw = lambda_raw / sum(lambda_raw)
+
+  phi = rlnorm(1,phi_prior_log_mean, phi_prior_log_sd)
+  alpha = rlnorm(1, alpha_prior_log_mean, alpha_prior_log_sd)
+
+  for(n in 1:N) {
+    counts[n,] = single_draw_nb_resample(lambda_raw * sums[n] * alpha, sums[n], phi)
+  }
+
+  data = list(
+    observed = list(
+      N = N,
+      G = G,
+      counts = counts,
+      phi_data = if(phi_given) { array(phi, 1) } else {numeric(0)},
+      phi_given = phi_given,
+      alpha_data = if(alpha_given) { array(alpha, 1) } else {numeric(0)},
+      alpha_given = alpha_given,
+      lambda_raw_prior = lambda_raw_prior
+    ),
+    true = list(lambda_raw = lambda_raw,
+                phi_param = if(!phi_given) { array(phi, 1) } else {numeric(0)},
+                alpha_param = if(!alpha_given) { array(alpha, 1) } else {numeric(0)}
+    )
+  )
+
+  if(phi_given) {
+    data$observed$phi_prior_log_mean = numeric(0)
+    data$observed$phi_prior_log_sd = numeric(0)
+  } else {
+    data$observed$phi_prior_log_mean = array(phi_prior_log_mean, 1)
+    data$observed$phi_prior_log_sd = array(phi_prior_log_sd, 1)
+  }
+
+  if(alpha_given) {
+    data$observed$alpha_prior_log_mean = numeric(0)
+    data$observed$alpha_prior_log_sd = numeric(0)
+  } else {
+    data$observed$alpha_prior_log_mean = array(alpha_prior_log_mean, 1)
+    data$observed$alpha_prior_log_sd = array(alpha_prior_log_sd, 1)
+  }
+
+  data
+
 }
--- stan/nb_multinomial_approx.stan
+++ stan/nb_multinomial_approx.stan
@@ -4,16 +4,16 @@ functions {
     z[1] = (exp(square(y[1])) - 1) * exp(2 * phi[1] + square(y[1])) - phi[2];
     return(z);
   }
-  
+
 }
 
 data {
   int<lower=0> N;
   int<lower=0> G;
   int<lower=0> counts[N,G];
-  real<lower=0> lambda_prior;
+  real<lower=0> lambda_raw_prior;
   int<lower=0,upper=1> phi_given;
-  
+
   vector<lower=0>[phi_given ? 1 : 0] phi_data;
   vector<lower=0>[phi_given ? 0 : 1] phi_prior_log_mean;
   vector<lower=0>[phi_given ? 0 : 1] phi_prior_log_sd;
@@ -28,8 +28,12 @@ transformed data {
   vector[1] mean_match_guess;
   real empty_real[0];
   int empty_int[0];
-  
+  int<lower=0> sums[N];
+
   mean_match_guess[1] = 1;//log(alpha + square(alpha / N) * N) - alpha;
+  for(n in 1:N) {
+    sums[n] = sum(counts[n,]);
+  }
 }
 
 parameters {
@@ -39,13 +43,13 @@ parameters {
 }
 
 transformed parameters {
-  vector<lower=0>[phi_given ? 0 : 1] phi_param = exp(phi_param_raw * phi_prior_log_sd + phi_prior_log_mean);
-  vector<lower=0>[alpha_given ? 0 : 1] alpha_param = exp(alpha_param_raw * alpha_prior_log_sd + alpha_prior_log_mean);
+  vector<lower=0>[phi_given ? 0 : 1] phi_param = exp(phi_param_raw .* phi_prior_log_sd + phi_prior_log_mean);
+  vector<lower=0>[alpha_given ? 0 : 1] alpha_param = exp(alpha_param_raw .* alpha_prior_log_sd + alpha_prior_log_mean);
   real phi = phi_given ? phi_data[1] : phi_param[1];
   real alpha = alpha_given ? alpha_data[1] : alpha_param[1];
   vector[G] lambda = lambda_raw * alpha;
   real<lower=0> phi_bar = phi * square(sum(lambda)) / (sum(square(lambda)));
-    
+
   //Note that alpha = sum(lambda)
   real nb_sum_variance = alpha + sum(square(lambda)) / phi;
 
@@ -54,7 +58,7 @@ transformed parameters {
   real sd_log_bar = sqrt(log(log_bar_helper));
 
   /* === Alternative way to determine params, seems to perform a tiny bit better, but is more complicated */
-  
+
   // real mean_log_bar = log(sum(lambda));
   // vector[2] solver_params = to_vector({mean_log_bar, nb_sum_variance});
   // real sd_log_bar = algebra_solver(
@@ -67,30 +71,30 @@ transformed parameters {
 }
 
 model {
-  lambda_raw ~ dirichlet(rep_vector(lambda_prior, G));
-  
+  lambda_raw ~ dirichlet(rep_vector(lambda_raw_prior, G));
+
   if(!alpha_given) {
     alpha_param_raw ~ normal(0,1);
   }
-  
+
   if(!phi_given) {
     phi_param_raw ~ normal(0, 1);
   }
-  
+
   for(n in 1:N) {
-    counts[n,] ~ neg_binomial_2(lambda, phi);
+    counts[n,] ~ neg_binomial_2(lambda * sums[n] , phi);
     //target += neg_binomial_2_lpmf(counts[n,] | lambda, phi);
     //target += -neg_binomial_2_lpmf(sum(counts[n,]) | alpha, phi_bar); //Note that alpha = sum(lambda)
     target += -lognormal_lpdf(sum(counts[n,]) | mean_log_bar, sd_log_bar);
-  } 
+  }
 }
 
 // generated quantities {
 //   vector[N] lnorm;
 //   vector[N] nb;
-//   
+//
 //   for(n in 1:N) {
 //     lnorm[n] = -lognormal_lpdf(sum(counts[n,]) | mean_log_bar, sd_log_bar);
 //     nb[n] = neg_binomial_2_lpmf(counts[n,]| lambda, phi);
-//   } 
+//   }
 // }
